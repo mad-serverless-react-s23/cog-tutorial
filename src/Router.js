7b47ea0ccaf1eadd281f4bc306f15becd3331085
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-09 15:39:21 -0500

    Update Router.js
    red squigs hope that's okay. videos will probably exchange it's deprecated or something. Oh check a different repository

--- src/Router.js
+++ src/Router.js
@@ -1,1 +1,38 @@
-// routing logic 
+// routing logic defines comps and routes in app
+// sets current route in Nav with window.location.href
+// HashRouter sync UI and URL
+// Switch gives only first child matching location
+// Route defines comp to render via path param
+import React, { useEffect, useState } from "react";
+import Public from './Public';
+import Profile from './Profile';
+import Protected from './Protected';
+import { HashRouter } from "react-router-dom";
+import { Switch } from "antd";
+
+export const Router = () => {
+    const [current, setCurrent] = useState('home');
+    useEffect(() => {
+        setRoute()
+        window.addEventListener('hashchange', setRoute);
+
+        return () => window.removeEventListener('hashchange', setRoute)
+    }, []);
+    const setRoute = () => {
+        const location = window.location.href.split('/');
+        const pathname = location[location.length-1];
+        setCurrent(pathname ? pathname : 'home');
+    };
+
+    return (
+        <HashRouter>
+            <Nav current={current} />
+            <Switch>
+                <Route exact path = "/" component={Public}/>
+                <Route exact path="/protected" component={Protected}/>
+                <Route exact path="/profie" component={Profile}/>
+                <Route component={Public}/>
+            </Switch>
+        </HashRouter>
+    );
+}
