0605ddc4d013aec50e5b105e592c4d30f8f51c2f
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-09 15:52:16 -0500

    Update Profile.js
    lots of stuff here, hope the function is properly "modern"

--- src/Profile.js
+++ src/Profile.js
@@ -1,2 +1,34 @@
 // render profile info for user.
 // add auth component for sign-up and sign-in
+// render auth form if not signed in, profile UI, sign-out button
+import React, { useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Container from './Container';
+
+const Profile = () => {
+    useEffect(() => {
+        checkUser()
+    }, []);
+    const [user, setUser] = useState({});
+    const checkUser = async() => {
+        try {
+            const data = await Auth.currentUserPoolUser();
+            const userInfo = { username: data.username, 
+                ...data.attributes, };
+            setUser(userInfo);
+        } catch (err) { console.log('error', err) };
+    };
+
+    return (
+        <Container>
+            <h1>Lucky you, you have a profile!</h1>
+            <h2>Your username is {user.userName}</h2>
+            <h3>You set your email to {user.email}</h3>
+            <h4>We don't intend to call you at {user.phone_number}</h4>
+            <AmplifySignOut />
+        </Container>
+    );
+};
+
+export default withAuthenticator(Profile);
