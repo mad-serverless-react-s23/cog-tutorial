314090ac30d782e2e2bca85ced22d9e4403d6121
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-09 14:26:53 -0500

    Update Container.js
    adjust any styles here for consistency in app

--- src/Container.js
+++ src/Container.js
@@ -1,1 +1,17 @@
 // hold component to apply reusable styling for components
+const Container = ({ children }) => {
+    <div style={StyleSheet.container}>
+        { children }
+    </div>
+};
+
+const styles = {
+    container: {
+        margin: '0 auto',
+        padding: '50px 100px'
+    }
+};
+
+export default Container;
+// all in <Container></Container> renders w/styles set in this file
+// adjust styles here for whole app
