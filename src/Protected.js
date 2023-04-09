e187e49e321fd39bb607a23dc3910ef7afa81a00
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-09 15:28:42 -0500

    Update Protected.js
    I think this will work...

--- src/Protected.js
+++ src/Protected.js
@@ -1,1 +1,21 @@
-// protected route seen by users, not-user redirect
+// protected route seen by users, not-user redirect to sign in/up
+// uses useEffect and Auth class
+// auth.current-blah in code checks for signed-in user
+import React, {useEffect} from 'react';
+import { Auth } from 'aws-amplify';
+import { Container } from './Container';
+
+export const Protected = (props) => {
+    useEffect(() => {
+        Auth.currentAuthenticatedUser()
+        .catch(() => {
+            props.histor.push('/profile')
+        })
+    }, []);
+
+    return (
+        <Container>
+            <h1>This area is under My Protection!</h1>
+        </Container>
+    );
+}
