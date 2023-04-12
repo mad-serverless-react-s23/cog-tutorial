17197ef56f8ac4ec865583a9058a02dde38d14cc
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-12 10:18:10 -0500

    Update Nav.js
    something doesn't look right but I trust the instructor more than the book

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,6 @@
 // create navigation ui
-import { Link } from "react-router-dom";
-import { Menu } from "antd";
+import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
 import {
     HomeOutlined,
     ProfileOutlined,
@@ -8,27 +8,27 @@ import {
 } from '@antd-design/icons';
 
 export const Nav = (props) => {
-    const { current } = props;
+    const { current } = props; // choice from user: where are we? what route?
     return (
         <div>
             <Menu selectedKeys={(current)} 
                 mode="horizontal">
-            <Menu.Item key='home>'>
-                <Link to={'/'}>
-                    <HomeOutlined />Home
-                </Link>
-            </Menu.Item>
-            <Menu.Item key='profile'>
-                <Link to='/profile'>
-                    <ProfileOutlined />Profile
-                </Link>
-            </Menu.Item>
-            <Menu.Item key='protected'>
-                <Link to='/protected'>
-                    <FileProtectOutlined />Protected
-                </Link>
-            </Menu.Item>
+                <Menu.Item key='home>'>
+                    <Link to={'/'}>
+                        <HomeOutlined />Home
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key='profile'>
+                    <Link to={'/profile'}>
+                        <ProfileOutlined />Profile
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key='protected'>
+                    <Link to={'/protected'}>
+                        <FileProtectOutlined />Protected
+                    </Link>
+                </Menu.Item>
             </Menu>
         </div>
-    )
+    ); // book does not use {} around to='/this'
 }
