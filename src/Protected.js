c4b96eb790c19aad7565f33b4541fe5012f47258
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-12 10:27:49 -0500

    Update Protected.js
    fixed typo

--- src/Protected.js
+++ src/Protected.js
@@ -1,17 +1,18 @@
 // protected route seen by users, not-user redirect to sign in/up
 // uses useEffect and Auth class
 // auth.current-blah in code checks for signed-in user
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Auth } from 'aws-amplify';
 import { Container } from './Container';
 
 export const Protected = (props) => {
     useEffect(() => {
         Auth.currentAuthenticatedUser()
-        .catch(() => {
-            props.histor.push('/profile')
-        })
-    }, []);
+            .catch(() => {
+                props.history.push('/profile') // no auth? go to profile
+            })
+        }, 
+    []);
 
     return (
         <Container>
