365468d6a04ece07391170d27b796301b2b0fdbb
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-15 15:53:31 -0500

    Update Protected.js
    example in github
    must be modernized

--- src/Protected.js
+++ src/Protected.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { Auth } from 'aws-amplify';
 import Container from './Container';
+import { useNavigate } from 'react-router-dom';
 
 const Protected = (props) => {
+    const navigate = useNavigate();
     useEffect(() => {
         Auth.currentAuthenticatedUser()
             .catch(() => {
-                props.history.push('/profile')
+                navigate('/profile')
             })
         }, 
     []);
