5de4a53beb6529f5991eaef0f821fe5d3d7596ce
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-20 16:15:45 -0500

    Update Profile.js
    this makes no sense. The only difference I see is the wording on my layout

--- src/Profile.js
+++ src/Profile.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Auth } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import Container from './Container';
 import '@aws-amplify/ui-react/styles.css';
+import Container from './Container';
 
 const Profile = () => {
     useEffect(() => {
         checkUser()
     }, []);
 
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState({})
 
     const checkUser = async() => {
         try {
@@ -22,11 +22,12 @@ const Profile = () => {
     return (
         <Container>
             <Authenticator>
-                {({ signOut, userInfo }) => (
+                {({ signOut, user }) => (
                     <main>
                         <h1>Lucky you, you have a profile!</h1>
                         <h2>Your username is {user.username}</h2>
                         <h3>You set your email to {user.email}</h3>
+                        <h4>We don't intend to call you at {user.phone_nomber}</h4>
                         <button onClick={signOut}>I don't want to be here!</button>
                     </main>
                 )}
