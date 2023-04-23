8072a92823993c71d1a6e08dcc4e46624184051d
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-22 23:37:39 -0500

    Update Public.js
    just having a bit of fun

--- src/Public.js
+++ src/Public.js
@@ -3,7 +3,8 @@ import Container from './Container';
 
 const Public = () => (
     <Container>
-        <h1>Public Here</h1>
+        <h1>Here is a beautiful public space.</h1>
+        <h2>Refreshingly blank for your sensory regulation.</h2>
     </Container>
 );
 export default Public
